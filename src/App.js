6639986cf6c70fd868258223d499c38ca3ac4537
PasemkoAndrii <pasemko.andrii@gmail.com>
2024-05-19 14:17:56 +0300

    2222

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsfwefwe
+          Learn Reactsfwefwwefe
         </a>
       </header>
     </div>
