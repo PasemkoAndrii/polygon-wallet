9944dd78378e16aa71e955c9cfb56067cebf9977
PasemkoAndrii <pasemko.andrii@gmail.com>
2024-05-19 14:54:35 +0300

    added web3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from "web3";
+import { useState } from "react";
+
+const web3 = new Web3(
+
+  "https://site1.moralis-nodes.com/amoy/30b0cdbc36154b09a5b40ec1e00cc8c9"
+
+);
+
 
 function App() {
+  const createAccount = async () => {
+    const res = web3.eth.accounts.create();
+    console.log(res);
+    setAccountAddr({ addr: res.address, key: res.privateKey });
+  };
+
+  const [accountAddr, setAccountAddr] = useState({ addr: 1, key: 2 });
+
   return (
     <div className="App">
       <h1>First Pageomopjopmopmpompomopomomomomoop</h1>
